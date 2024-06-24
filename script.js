c3d5a8deb2076b7431c5537c5b5cdf6a796f3902
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2024-06-24 17:39:12 +0200

    setting up "Section Obsever"

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  const observerOptions = {
+    root: null, // viewport
+    rootMargin: '400px',
+    threshold: 0.5, // 50% of section must be visible
+  };
+
+  const sectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+        // const bgColor = getComputedStyle(entry.target).backgroundColor;
+        document.body.style.color = '#d5c3fd';
+        document.body.style.backgroundColor = 'black';
+      } else {
+        document.body.style.backgroundColor = '#d5c3fd';
+        document.body.style.color = 'black';
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => {
+    sectionObserver.observe(section);
+  });
+});
