0514e4352d59d300dcad01485009f2f3c272bee2
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2024-06-25 23:32:35 +0200

    working on intersecOb

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('section');
+  const sections = document.querySelectorAll(
+    '[data-my-color][data-my-text-color]'
+  );
+  let currentSection = null;
 
   const observerOptions = {
     root: null, // viewport
-    // rootMargin: '400px',
+    rootMargin: '-60px',
     threshold: 0.5, // 50% of section must be visible
   };
 
   const sectionObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
-        // const bgColor = getComputedStyle(entry.target).backgroundColor;
-        document.body.style.color = '#d5c3fd';
-        document.body.style.backgroundColor = 'black';
-        section2_awareContainer.style.color = '#d5c3fd';
-      } else {
-        document.body.style.backgroundColor = '#d5c3fd';
-        document.body.style.color = 'black';
+        const bgColor = entry.target.dataset.myColor;
+        const textColor = entry.target.dataset.myTextColor;
+        document.body.style.backgroundColor = bgColor;
+        document.body.style.color = textColor;
+        currentSection = entry.target;
+      } else if (entry.target === currentSection && !entry.isIntersecting) {
+        // Reset colors only if the current section is no longer intersecting
+        document.body.style.backgroundColor = '';
+        document.body.style.color = '';
+        currentSection = null;
       }
     });
   }, observerOptions);
